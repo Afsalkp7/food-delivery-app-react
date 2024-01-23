4a9f5fa3be70bef9bb9c84c68875bf23a4270f4e
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-01-23 20:48:46 +0530

    setup completed

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+
+const header = <h1>hello world !!!</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(header)
