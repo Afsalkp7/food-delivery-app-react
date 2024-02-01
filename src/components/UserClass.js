c59b3d6e8e200b31bb97695390855816c3468784
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-02-01 16:00:35 +0530

    update state variable in class component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,6 +3,9 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0
+    };
   }
   render() {
     const {name,place} = this.props
@@ -10,6 +13,10 @@ class UserClass extends React.Component {
       <div>
         <h1>full name : {name}</h1>
         <h2>place : {place}</h2>
+        <h3>count fron class : {this.state.count}</h3>
+        <button onClick={()=>{
+          this.setState({count:this.state.count+1})
+        }}>Counter</button>
       </div>
     );
   }
--- src/components/UserFunction.js
+++ src/components/UserFunction.js
@@ -1,8 +1,17 @@
+import { useState } from 'react';
+
 const User = (props)=>{
-    return 
-    (<div>
+    const [count,setCount] = useState(0);
+
+    return (<div>
         <h1>full name : {props.name}</h1>
         <h2>place : {props.place}</h2>
+        <h3>Count from function : {count}</h3>
+        <button onClick={
+            ()=>{
+                setCount(count+1)
+            }
+        }>Counter</button>
     </div>)
 }
 export default User
